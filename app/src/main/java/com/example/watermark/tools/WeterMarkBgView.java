9bba0258a1ad05962a8d0e3d72df5600bb18dfd8
DESKTOP-7U5ERIR\boyan.lin <boyan.lin@flowring.com>
2023-09-20 10:55:41 +0800

    設置圖片透明度和文字字體以及調整位置

--- app/src/main/java/com/example/watermark/tools/WeterMarkBgView.java
+++ app/src/main/java/com/example/watermark/tools/WeterMarkBgView.java
@@ -1,24 +1,31 @@
 package com.example.watermark.tools;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PixelFormat;
+import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.example.watermark.R;
+
 import java.util.List;
 
 public class WeterMarkBgView extends Drawable {
     private Paint paint = new Paint();
+    private Paint bitmapPaint = new Paint();
     private List<String> labels;
     private Context context;
     private int degress;   //角度
     private int fontSize;  // 字體大小，單位sp
+    private Typeface customTypeface;
     /*
     context  上下文
     labels   水印文字列表
@@ -31,13 +38,14 @@ public class WeterMarkBgView extends Drawable {
         this.context = context;
         this.degress = degress;
         this.fontSize = fontSize;
+        customTypeface = Typeface.createFromAsset(context.getAssets(), "SentyDew.ttf");
     }
 
     @Override
     public void draw(@NonNull Canvas canvas) {
-        //getBounds().right 和 getBounds().bottom 獲取繪製區域的寬度和高度。
-        int width = getBounds().right;
-        int height = getBounds().top + 200;
+        // 計算畫布的寬度和高度
+        int canvasWidth = getBounds().width();
+        int canvasHeight = getBounds().height();
 
         //畫布背景色
         canvas.drawColor(Color.parseColor("#40F3F5F9"));
@@ -47,16 +55,36 @@ public class WeterMarkBgView extends Drawable {
         paint.setAntiAlias(true);
         // setTextSize方法設置水印文字的大小，使用 sp2px 方法將傳入的字體大小轉換為像素值。
         paint.setTextSize(sp2px(context,fontSize));
+        // 設置字體
+        paint.setTypeface(customTypeface);
+        // 設置透明度為半透明
+        paint.setAlpha(255);
+        // 設置圖片透明度
+        Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.call_cancel_icon);
+        bitmapPaint.setAlpha(255);
+
+        // 計算圖片的左上角位置，使它們都位於畫布中心
+        int imageLeft = (canvasWidth - bitmap.getWidth()) / 2;
+        int imageTop = (canvasHeight - bitmap.getHeight()) / 2;
+        canvas.drawBitmap(bitmap, imageLeft, imageTop, bitmapPaint);
+
         // 保存當前的繪製狀態
         canvas.save();
-        // rotate 方法將整個畫布旋轉指定的角度（degress）。
-        canvas.rotate(degress);
+
+        // 獲取水印文字的寬度和高度
         // measureText 方法測量第一個水印文字（labels.get(0)）的寬度，將結果存儲在 textWidth 變量中
         float textWidth = paint.measureText(labels.get(0));
+        float textHeight = paint.getTextSize();
+        // 計算文字的左上角位置，使它們都位於畫布中心
+        int textLeft = (canvasWidth - (int)textWidth) / 2;
+        int textTop = (canvasHeight + (int)textHeight) / 2;
+
+        // rotate 方法將整個畫布旋轉指定的角度（degress），以下改成旋轉文字角度
+        canvas.rotate(degress, textLeft + textWidth / 2, textTop - textHeight / 2);
 
         int spacing = 0; // 垂直間距，drawText(要繪製的文本內容,x軸起始位置畫布左上角,y軸起始位置畫布左上角,Paint對象設置文本的顏色)
         for (String label : labels) {
-            canvas.drawText(label, 100, height+ spacing, paint);
+            canvas.drawText(label, textLeft, textTop  + spacing, paint);
             spacing = spacing + 100;
         }
 
